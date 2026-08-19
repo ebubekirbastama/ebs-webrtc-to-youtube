002fa275bb48c0814d395dc92e724d85d6eac5a6
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2026-08-19 09:00:31 +0300

    Delete examples directory

--- examples/linux-logo.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-python3 src/webrtc_to_youtube.py \
-  --logo assets/logo.png \
-  --logo-position top-left \
-  --logo-width 200 \
-  --logo-opacity 1.0
--- examples/windows-logo.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-python src\webrtc_to_youtube.py ^
-  --logo assets\logo.png ^
-  --logo-position top-left ^
-  --logo-width 200 ^
-  --logo-opacity 1.0
-pause
